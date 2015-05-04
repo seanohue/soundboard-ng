389171087396527f9dc6ce3a8338106b1b18debe
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-04 11:22:54 -0400

    The controller is working but ng-if, ng-hide, and ng-show have failed me.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,6 +5,13 @@
 	scControllers.controller('DisplayController', ['$scope', function($scope) {
 	  var vm = this;
 	  vm.greeting = 'SoundBoard';
+	  vm.playingNow = false;
+	  vm.togglePlay = function(){
+	  	console.log(vm.playingNow);
+	  	console.log("toggling so hard right now")
+	  	vm.playingNow = !vm.playingNow;
+	  	console.log(vm.playingNow);
+	  };
 	}]);
 
 })();
