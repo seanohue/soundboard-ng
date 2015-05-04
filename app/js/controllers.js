c2087b5326b7675089066457799cc2decaaaf74b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-04 11:54:08 -0400

    Adds function to return playing status and now toggling works, woooooooooooooooooooooooooooo party exclamation point.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,17 @@
 	
 	scControllers.controller('DisplayController', ['$scope', function($scope) {
 	  var vm = this;
-	  vm.greeting = 'SoundBoard';
 	  vm.playingNow = false;
+	  vm.greeting = 'SoundBoard';
 	  vm.togglePlay = function(){
 	  	console.log(vm.playingNow);
 	  	console.log("toggling so hard right now")
 	  	vm.playingNow = !vm.playingNow;
 	  	console.log(vm.playingNow);
 	  };
+	  vm.nowPlaying = function(){
+	  	return vm.playingNow;
+	  }
 	}]);
 
 })();
