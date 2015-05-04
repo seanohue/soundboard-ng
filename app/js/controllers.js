93ae2bfab612290b57049bccc04289a58bebd39c
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-04 11:57:48 -0400

    Removes console logs and does whitespace stuff. What a boring commit.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,17 @@
 (function(){
 	var scControllers = angular.module('scControllers', [
 		]);
-	
+
 	scControllers.controller('DisplayController', ['$scope', function($scope) {
 	  var vm = this;
 	  vm.playingNow = false;
 	  vm.greeting = 'SoundBoard';
 	  vm.togglePlay = function(){
-	  	console.log(vm.playingNow);
-	  	console.log("toggling so hard right now")
 	  	vm.playingNow = !vm.playingNow;
-	  	console.log(vm.playingNow);
 	  };
 	  vm.nowPlaying = function(){
 	  	return vm.playingNow;
 	  }
 	}]);
-
+	
 })();
