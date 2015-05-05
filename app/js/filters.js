c257d88219404574ca23b5266a1cfda027813b6e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-05 14:24:21 -0400

    Notes in filter on how to solve. Also, used limit-to to make it prettier. Wooooo.

--- app/js/filters.js
+++ app/js/filters.js
@@ -9,4 +9,5 @@
 		//I would need to find out if something is playing or not.
 		//Maybe I could just autoplay the first track on Now Playing.
 		//hmmm
+		//set current index (pass in $index with a function on click, stored in controller. controller has songplaying variable and use limitTo: 1)
 	}]);
