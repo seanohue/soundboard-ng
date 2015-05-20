b96262231e5d0ecccd7aae627c936f5cf6d8a246
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-20 13:01:10 -0400

    Removes some spaghetti code and dead code.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,7 @@
 		'$scope', 
 		'SearchService',
 		function($scope, SearchService) {
+	  
 	  var vm = this;
 	  $scope.playingNow = false;
 	  vm.greeting = 'SoundBoard';
@@ -47,46 +48,37 @@
 		'SearchService',
 		'$timeout',
 		function($scope, SearchService, $timeout) {
+		
 		var vm = this;
 		vm.term = "";
 		vm.results = [];
 		vm.searchFilter = "Artists";
+
 		vm.change = function(newSearch){
 			vm.searchFilter = newSearch;
 		};
+
 		vm.category = function(){
 			return vm.searchFilter;
 		};
+
 		vm.getResults = function(searchFor){
 			var searchFor = searchFor.toLowerCase();
 			console.log(vm.searchFilter);
 			var cat = vm.searchFilter.toLowerCase();
-			// had to change this bit to include data, and make a .then call
 			SearchService.getSearchText(searchFor, cat).then(function(data) {
                 vm.results = data;
             });
 			return vm.results;
 		};
-		//this function is no longer necessary, i think
-		vm.getSearchText = function(searchFor){
-			var searchFor = searchFor.toLowerCase();
-			console.log(vm.searchFilter);
-			var cat = vm.searchFilter.toLowerCase();	
-			$timeout(function() {
-				vm.results = SearchService.getSearchText(searchFor, cat);
-				console.log("recorded results are: " + vm.results);
-				console.log("timeout is working");
-				//$scope.$apply();
-        		//return vm.results;
-      		}, 200);
-		};
-
+	
 		vm.setSelection = function(selectionURL){
 	  		console.log("pres butan " + selectionURL);
 	  		SearchService.setSelection(selectionURL);
 	  	};
 
 		vm.searched = false;
+		
 		vm.toggleSearched = function(){
 			vm.searched = !vm.searched;
 		}
