9f17e6f0fbba7065605d94d018d03d77dd3cba95
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-05-21 09:59:26 -0400

    Adds functionality to controller for incrementing/decrementing the playlist counter and links it to the view. Now to get rid of ng-repeat, somehow........

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,11 +13,33 @@
 	  vm.greeting = 'SoundBoard';
 	  vm.trackCounter = 0; //use this to keep track rather than $index
 
-	  vm.togglePlay = function(){
-	  	console.log(vm.playingNow);
-	  	console.log("toggling so hard right now");
-	  	vm.playingNow = !vm.playingNow;
-	  	console.log(vm.playingNow);
+	  vm.nextTrack = function(){
+	  	vm.trackCounter++;
+	  	console.log('next');
+	  };
+
+	  vm.previousTrack = function(){
+	  	vm.trackCounter--;
+	  	console.log('last');
+	  };
+
+	  vm.resetCounter = function(){
+	  	vm.trackCounter = 0;
+	  	console.log('reset');
+	  }
+
+	  vm.togglePlay = function(status){
+	  	if (status != undefined){
+		  	console.log(vm.playingNow);
+		  	console.log("toggling so hard right now");
+		  	vm.playingNow = !vm.playingNow;
+		  	console.log(vm.playingNow);
+	  	}
+	  	else
+	  		{
+	  			vm.playingNow = status;
+	  			console.log('status is '+status+' and play is set to '+vm.togglePlay);
+	  		};
 	  };
 
 	  vm.getSelection = function(){
